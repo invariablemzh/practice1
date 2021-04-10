0df21d32dfb2b627a09adb15573ba707f0603303
invariablemzh <81310534+invariablemzh@users.noreply.github.com>
2021-04-10 23:45:44 +0800

    Update (递归，分形）AcWing 98. 分形之城.cpp

--- (递归，分形）AcWing 98. 分形之城.cpp
+++ (递归，分形）AcWing 98. 分形之城.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-pair<long long,long long> calc(long long n,long long m){
+pair<ll,ll> calc(ll n,ll m){
     if(n==0) return make_pair(0,0);
-    long long len = 1ll<<(n-1),cnt = 1ll << (2*n-2);
-    pair<long long,long long> pos = calc(n-1,m%cnt);
-    long long x = pos.first,y = pos.second;
-    long long z = m/cnt;
-    if(z==0) return make_pair(y,x);
-    if(z==1) return make_pair(x,y+len);
-    if(z==2) return make_pair(x+len,y+len);
-    return make_pair(2*len-1-y,len-x-1);
+    ll len = 1ll<<(n-1),cnt = 1ll<<(2*n-2);
+    pair<ll,ll> pos = calc(n-1,m%cnt);//得到n-1时的坐标
+    ll x = pos.first,y = pos.second;
+    ll z = m/cnt;
+    if(z==0) return make_pair(y,x);//顺时针，用矩阵，（0，0）为起点
+    else if(z==1) return make_pair(x,y+len);
+    else if(z==2) return make_pair(x+len,y+len);
+    else return make_pair(2*len-y-1,len-x-1);//旋转+翻转，
 }
+
 
 int main(){
     int t;
     cin>>t;
     while(t--){
-        long long n,a,b;
+        ll n,a,b;
         cin>>n>>a>>b;
-        pair<long long,long long> x = calc(n,a-1);
-        pair<long long,long long> y = calc(n,b-1);
-        long long dx = x.first-y.first,dy = x.second-y.second;
-        double ans = (sqrt(dx*dx+dy*dy)*10);
-        printf("%0.lf\n",ans);
+        auto ac = calc(n,a-1);
+        auto bc = calc(n,b-1);
+        ll dx = ac.first-bc.first;
+        ll dy = ac.second-bc.second;
+        double ans = sqrt(dx*dx+dy*dy)*10;
+        printf("%.0lf\n",ans);
     }
     return 0;
 }
